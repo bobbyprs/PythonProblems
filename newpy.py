0624e293530540404f76bcb8c1b7f6b57a229f82
bobbyprs <saikumar3512@gmai.com>
2021-03-24 12:27:52 +0530

    first commit

---
+++ newpy.py
@@ -0,0 +1,19 @@
+# def sum(numbers):
+#     total = 0
+#     for x in range(numbers):
+#         total += x
+#     return total
+# print(sum((11)))
+# ###########################################
+# list1 = [8, 15, 32, 42, 60, 75, 122, 132, 150, 180, 190]
+# for i in list1:
+#     if i%4==0 and i<120 :
+#         print(i)
+##############################
+num1 = [5, 15, 20, 25, 30]
+# num1.reverse()
+# print(num1)
+# num1=num1[::-1]
+# print(num1)
+for i in list(reversed(num1)):
+    print(i)
